e48320a779449aa92cff71cbcc869c8f6337f566
Siarhei Karobka <Okborok1@gmail.com>
2018-12-03 15:56:20 +0300

    add base project files

---
+++ src/main/java/util/DBWorker.java
@@ -0,0 +1,97 @@
+package util;
+
+import com.mysql.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBWorker {
+
+    private final String URL = "jdbc:mysql://localhost:3306/phonebook?useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private final String USERNAME = "root";
+    private final String PASSWORD = "root";
+
+    // Количество рядов таблицы, затронутых последним запросом.
+    private Integer affected_rows = 0;
+
+    // Значение автоинкрементируемого первичного ключа, полученное после
+    // добавления новой записи.
+    private Integer last_insert_id = 0;
+
+    // Указатель на экземпляр класса.
+    private static DBWorker instance = null;
+
+    // Метод для получения экземпляра класса (реализован Singleton).
+    public static DBWorker getInstance() {
+        if (instance == null) {
+            instance = new DBWorker();
+        }
+
+        return instance;
+    }
+
+    // "Заглушка", чтобы экземпляр класса нельзя было получить напрямую.
+    private DBWorker() {
+        // Просто "заглушка".
+    }
+
+    // Выполнение запросов на выборку данных.
+    public ResultSet getDBData(String query) {
+        Statement statement;
+        Connection connect;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connect = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            statement = connect.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            return resultSet;
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("null on getDBData()!");
+        return null;
+
+    }
+
+    // Выполнение запросов на модификацию данных.
+    public Integer changeDBData(String query) {
+        Statement statement;
+        Connection connect;
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            connect = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            statement = connect.createStatement();
+            this.affected_rows = statement.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+
+            // Получаем last_insert_id() для операции вставки.
+            ResultSet rs = statement.getGeneratedKeys();
+            if (rs.next()) {
+                this.last_insert_id = rs.getInt(1);
+            }
+
+            return this.affected_rows;
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("null on changeDBData()!");
+        return null;
+    }
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++
+    // Геттеры и сеттеры.
+    public Integer getAffectedRowsCount() {
+        return this.affected_rows;
+    }
+
+    public Integer getLastInsertId() {
+        return this.last_insert_id;
+    }
+    // Геттеры и сеттеры.
+    // -------------------------------------------------
+}
+
